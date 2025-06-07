b03051e272cf37868ea92de266b8edf042c1476b
agroon280 <caurrodrigo@gmail.com>
2025-06-07 18:41:20 +0000

    habilidade

--- battnavalaventur.c
+++ battnavalaventur.c
@@ -3,11 +3,11 @@
 int main() {
 char letras[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 int tabuleiro[10][10];
+
 //iniciar dando valor a tabela
 for (int i = 0; i < 10; i++){
 for(int j = 0;j<10;j++){
-tabuleiro[i][j]=0;
-}}
+tabuleiro[i][j]=0;}}
 
 //inicializando os navios
 for (int i = 0; i < 3; i++){
@@ -16,6 +16,36 @@ tabuleiro[i][9 - i]=3;}
 for(int i = 0; i < 1; i++){
     for (int j = 0; j < 3; j++){
 tabuleiro[i+3][j]=3;}}
+
+//habilidades especiais piramide
+int inicio = 4;//diz qual linha
+int centro = 5;//diz ao codigo aonde comecar na linha
+
+for (int i = 0; i < 3; i++) {
+
+int linha = inicio + i;
+if (linha >= 10) break;//nao sair da matriz
+
+for (int j = centro - i; j <= centro + i; j++) {
+//diz qual e a linha scaneada
+if (j >= 0 && j < 10) {
+tabuleiro[linha][j] = 5; // marca dano
+}}
+}
+
+//cruz
+int linhcruz = 1;
+int colunacruz = 1;
+
+for(int i= 0; i < 3; i++){
+tabuleiro[linhcruz][i] = 5;}
+
+for(int i = 0; i < 3; i++){
+tabuleiro[i][colunacruz] = 5;
+}
+
+//octaedro
+alcance = 
 
 //imprimir o nome do jogo
 printf("BATALHA NAVAL\n");//pula para outra linha
